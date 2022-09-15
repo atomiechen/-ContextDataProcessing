29018061fcab4d1cea50383eed9b0d6491c5356a
Atomie CHEN <atomic_cwh@163.com>
2022-09-15 17:09:07 +0800

    Fix and improve merge script

--- merge_log.py
+++ merge_log.py
@@ -1,10 +1,13 @@
 import argparse
 import os
-from datetime import datetime
 from tqdm import tqdm
 
 
 USER_LOG_FMT = "merged_{}.log"
+
+def mkdir(path):
+	if not os.path.exists(path):
+		os.makedirs(path, exist_ok=True)
 
 def get_basename(path):
 	path, tail = os.path.split(path)
@@ -18,61 +21,60 @@ def write_log_file(filename, fout):
 			if line.strip() != "":
 				fout.write(line)
 
-def traverseRootFolder(dirpath, output_dirpath):
-	if not os.path.isdir(dirpath) or not os.path.isdir(output_dirpath):
+def merge_for_each_subfolder(dirpath, output_dirpath):
+	if not os.path.isdir(dirpath):
 		return
 	for user in os.listdir(dirpath):
-		traverseUserFolder(os.path.join(dirpath, user), output_dirpath)
+		merge_folder(os.path.join(dirpath, user), output_dirpath)
 
-def traverseUserFolder(dirpath, output_dirpath):
-	if not os.path.isdir(dirpath) or not os.path.isdir(output_dirpath):
-		return
-	folder_list = []
-	for item in os.listdir(dirpath):
-		if os.path.isdir(os.path.join(dirpath, item)):
-			try:
-				## parse to check validity
-				date_object = datetime.strptime(item, "%Y-%m-%d")
-				# print(date_object)
-				folder_list.append(item)
-			except:
-				pass
-	if len(folder_list) > 0:
-		folder_list.sort()
-		user = get_basename(dirpath)
-		print(f"Merging user folder: {user}")
-		print(folder_list)
-		output_filepath = os.path.join(output_dirpath, USER_LOG_FMT.format(user))
-		with open(output_filepath, 'w', encoding='utf-8') as fout:
-			for item in tqdm(folder_list):
-				traverseDateFolder(os.path.join(dirpath, item), fout)
-
-def traverseDateFolder(dirpath, fout):
+def merge_folder(dirpath, output_dirpath):
 	if not os.path.isdir(dirpath):
 		return
 	file_list = []
-	all_files = os.listdir(dirpath)
+	traverse_foler(dirpath, file_list)
+	if len(file_list) > 0:
+		file_list.sort()
+		user = get_basename(dirpath)
+		mkdir(output_dirpath)
+		output_filepath = os.path.join(output_dirpath, USER_LOG_FMT.format(user))
+
+		print(f"Merging folder: {user}")
+		print(f"Output to: {output_filepath}")
+		print(f"Need to merge {len(file_list)} log files")
+		
+		merge_logs(file_list, output_filepath)
+
+def traverse_foler(dirpath, file_list):
+	if not os.path.isdir(dirpath):
+		return
+	all_files = set(os.listdir(dirpath))
 	for item in all_files:
-		## check if valid log file
-		if os.path.isfile(os.path.join(dirpath, item)) and item + ".meta" in all_files:
-			file_list.append(item)
-	file_list.sort()
-	for filename in file_list:
-		write_log_file(os.path.join(dirpath, filename), fout)
+		full_path = os.path.join(dirpath, item)
+		if os.path.isdir(full_path):
+			## check if folder
+			traverse_foler(full_path, file_list)
+		elif item + ".meta" in all_files:
+			## check if valid log file
+			file_list.append(full_path)
+
+def merge_logs(file_list, output_filepath):
+	with open(output_filepath, 'w', encoding='utf-8') as fout:
+		for filename in tqdm(file_list):
+			write_log_file(filename, fout)
 
 def main(args):
 	dirpath = args.dirpath
 	output_dirpath = args.output
-	if args.user:
-		traverseUserFolder(dirpath, output_dirpath)
+	if args.subfolder:
+		merge_for_each_subfolder(dirpath, output_dirpath)
 	else:
-		traverseRootFolder(dirpath, output_dirpath)
+		merge_folder(dirpath, output_dirpath)
 
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, description="Merge log files")
 	parser.add_argument('dirpath', action='store', help="Directory path")
-	parser.add_argument('-u', dest='user', action='store_true', default=False, help="start from user folder")
+	parser.add_argument('--sub', dest='subfolder', action='store_true', default=False, help="merge each subfolder to individual log file")
 	parser.add_argument('-o', dest='output', action='store', default=".", help="output directory path")
 
 	args = parser.parse_args()
