518666bd2542a593b6517025c2abe0fd181a12a0
Atomie CHEN <atomic_cwh@163.com>
2022-09-17 18:46:57 +0800

    Add script to append date time in log

---
+++ parse_log.py
@@ -0,0 +1,31 @@
+import argparse
+import os
+from datetime import datetime
+
+def parse_line(line):
+	paras = line.strip().split('\t')
+	timestamp = int(paras[0])
+	date_object = datetime.fromtimestamp(timestamp/1000)
+	return date_object.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+
+def parse_log(filepath):
+	head, tail = os.path.split(filepath)
+	out_filepath = os.path.join(head, "parsed_" + tail)
+	print(f'Reading from {filepath}')
+	print(f'Writing to {out_filepath}')
+	with open(filepath, 'r', encoding='utf-8') as fin:
+		with open(out_filepath, 'w', encoding='utf-8') as fout:
+			for line in fin:
+				date_str = parse_line(line)
+				fout.write(date_str + '\t' + line)
+
+def main(args):
+	for filepath in args.filepath:
+		parse_log(filepath)
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, description="Append datetime to log file")
+	parser.add_argument('filepath', nargs='+', action='store', help="Input file path; can have multiple paths")
+	args = parser.parse_args()
+	main(args)
