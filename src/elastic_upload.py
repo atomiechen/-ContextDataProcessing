1729bb16f2ccb1304766f24f5aaf5958f5f95a43
Atomie CHEN <atomic_cwh@163.com>
2022-09-20 12:38:37 +0800

    Support uploading a single log file

--- src/elastic_upload.py
+++ src/elastic_upload.py
@@ -1,39 +1,80 @@
 import argparse
 import yaml
 from elasticsearch import Elasticsearch
+from elasticsearch.helpers import streaming_bulk
+import tqdm
 
 
-def load_config(filename):
-	with open(filename, 'r', encoding='utf-8') as fin:
+def load_config(filepath):
+	with open(filepath, 'r', encoding='utf-8') as fin:
 		config = yaml.safe_load(fin)
 	## 填充缺省值
+	if 'elastic' not in config:
+		config['elastic'] = {}
+	cfg_elastic = config['elastic']
 	## 默认用户名为elastic
-	if 'username' not in config:
-		config['username'] = 'elastic'
+	if 'username' not in cfg_elastic:
+		cfg_elastic['username'] = 'elastic'
 	## 默认检查服务器证书
-	if 'verify_certs' not in config:
-		config['verify_certs'] = True
+	if 'verify_certs' not in cfg_elastic:
+		cfg_elastic['verify_certs'] = True
 	## 默认证书地址为当前目录下的http_ca.crt
-	if 'certificate_path' not in config:
-		config['certificate_path'] = 'http_ca.crt'
+	if 'certificate_path' not in cfg_elastic:
+		cfg_elastic['certificate_path'] = 'http_ca.crt'
 	return config
+
+## ref: https://stackoverflow.com/a/27518377/11854304
+def count_lines(filepath):
+	def _make_gen(reader):
+		b = reader(1024 * 1024)
+		while b:
+			yield b
+			b = reader(1024*1024)
+	with open(filepath, 'rb') as f:
+		f_gen = _make_gen(f.raw.read)
+		return sum( buf.count(b'\n') for buf in f_gen )
+
+def gen_doc(filepath, userid):
+	with open(filepath, 'r', encoding='utf-8') as fin:
+		for line in fin:
+			## remove empty lines
+			if line.strip() != "":
+				yield {"message": line, "userid": userid}
 
 def main(args):
 	config = load_config(args.config)
+	cfg_elastic = config['elastic']
 	# Create the client instance
 	client = Elasticsearch(
-		config['host'],
-		verify_certs=config['verify_certs'],
-		ca_certs=config['certificate_path'],
-		basic_auth=(config['username'], config['password'])
+		cfg_elastic['host'],
+		verify_certs=cfg_elastic['verify_certs'],
+		ca_certs=cfg_elastic['certificate_path'],
+		basic_auth=(cfg_elastic['username'], cfg_elastic['password'])
 	)
 
 	# Successful response!
 	print(client.info())
 
-	doc = {
-		'message': '1662724635868	36	KeyEvent	KeyEvent://1/25		{\"keycodeString\":\"KEYCODE_VOLUME_DOWN\",\"package\":\"\",\"action\":1,\"source\":257,\"code\":25,\"eventTime\":2583640595,\"downTime\":2583640463}'
-	}
+	## get data info from config
+	filepath = config['data']['filepath']
+	userid = config['data']['userid']
+
+	number_of_docs = count_lines(filepath)
+	successes = 0
+	progress = tqdm.tqdm(unit="docs", total=number_of_docs)
+	for success, info in streaming_bulk(
+		client=client, actions=gen_doc(filepath, userid), index='log_volume'):
+		progress.update(1)
+		successes += success
+		if not success:
+			print('A document failed:', info)
+	
+	print(f"Indexed {successes}/{number_of_docs} documents")
+
+
+	# doc = {
+	# 	'message': '1662724635868	36	KeyEvent	KeyEvent://1/25		{\"keycodeString\":\"KEYCODE_VOLUME_DOWN\",\"package\":\"\",\"action\":1,\"source\":257,\"code\":25,\"eventTime\":2583640595,\"downTime\":2583640463}'
+	# }
 	# resp = client.index(index="log_volume", document=doc)
 	# print(resp)
 
