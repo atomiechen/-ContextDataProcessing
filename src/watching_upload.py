30abc88b482678ae08b2df541cc4a1ad5ed47c4d
Atomie CHEN <atomic_cwh@163.com>
2022-09-21 17:12:14 +0800

    Upload all files in watching folder on startup

--- src/watching_upload.py
+++ src/watching_upload.py
@@ -4,6 +4,7 @@ import time
 import json
 from datetime import datetime
 from concurrent.futures import Executor, ThreadPoolExecutor
+import threading
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler, FileSystemEvent
 
@@ -16,6 +17,7 @@ class UploadHandler(FileSystemEventHandler):
 		super().__init__()
 		self.uploader = ElasticUploader(config)
 		self.executor = executor
+		self.set_lock = threading.Lock()
 		
 		## read in uploaded file list
 		try:
@@ -28,15 +30,13 @@ class UploadHandler(FileSystemEventHandler):
 
 
 	def on_any_event(self, event: FileSystemEvent):
-		print(f'{datetime.now(tz=Beijing_timezone)}  Event: {event.event_type}  Path: {event.src_path}')
+		print(f'{datetime.now(tz=Beijing_timezone)} Event: {event.event_type} Path: {event.src_path}')
 		if event.event_type == 'created':
-			ft = self.executor.submit(self.check_and_upload, event.src_path)
+			### 受到新建文件事件后，总是重新上传以更新文档，因此force_upload=True
+			ft = self.executor.submit(self.check_and_upload, event.src_path, True)
 
-	def check_and_upload(self, path: str):
+	def check_and_upload(self, path: str, force_upload=False):
 		abs_path = os.path.abspath(path)
-		### 受到新建文件事件后，总是重新上传以更新文档，因此注释下面这段代码
-		# if abs_path in self.uploaded_set:
-		# 	return
 		if os.path.isfile(abs_path):
 			tmp_add_meta = abs_path + ".meta"
 			tmp_rm_meta = abs_path[:-5]
@@ -51,25 +51,47 @@ class UploadHandler(FileSystemEventHandler):
 				abs_path = tmp_rm_meta
 				need_upload = True
 			
+			## (no forcing upload) if already uploaded, skip it
+			if not force_upload and abs_path in self.uploaded_set:
+				return
+
 			## valid file and need to upload
 			if need_upload:
 				## get user ID from meta file
 				try:
-					print(f"Try to upload {abs_path}")
 					with open(meta_path, 'r', encoding='utf-8') as fin:
 						data = json.load(fin)
 					userid = data['userId']
 					if self.uploader.upload(abs_path, userid):
-						self.uploaded_set.add(abs_path)
-						self.write_uploaded_list()
-					print(f"Uploaded: {abs_path}")
-				except:
-					pass
+						## record this uploaded file
+						with self.set_lock:
+							self.uploaded_set.add(abs_path)
+							self.write_uploaded_list()
+						print(f"{datetime.now(tz=Beijing_timezone)} Uploaded: {abs_path}")
+					else:
+						print(f"{datetime.now(tz=Beijing_timezone)} Failed to upload: {abs_path}")
+				except Exception as e:
+					print(f"{datetime.now(tz=Beijing_timezone)} Failed to upload: {abs_path}, with exception: {e}")
 
 	def write_uploaded_list(self):
 		with open(self.uploaded_list_path, 'w', encoding='utf-8') as fout:
 			for item in self.uploaded_set:
 				fout.write(item + "\n")
+
+	def schedule_upload_folder(self, dirpath):
+		return self.executor.submit(self.traverse_folder, dirpath).add_done_callback(
+			lambda ft: print(f"{datetime.now(tz=Beijing_timezone)} Upload folder done.")
+			)
+
+	def traverse_folder(self, dirpath):
+		if not os.path.isdir(dirpath):
+			return
+		for item in os.listdir(dirpath):
+			full_path = os.path.join(dirpath, item)
+			if os.path.isdir(full_path):
+				self.traverse_foler(full_path)
+			else:
+				self.check_and_upload(full_path)
 
 
 def main(args):
@@ -80,7 +102,9 @@ def main(args):
 		observer = Observer()
 		observer.schedule(event_handler, watching_path, recursive=True)
 		observer.start()
-		print(f"Watching for path: {watching_path}")
+		print(f"{datetime.now(tz=Beijing_timezone)} Watching for path: {watching_path}")
+		print(f"{datetime.now(tz=Beijing_timezone)} Uploading files in path: {watching_path}")
+		event_handler.schedule_upload_folder(watching_path)
 		try:
 			while True:
 				time.sleep(1)
