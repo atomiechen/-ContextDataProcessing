de4d3deab84a29d0528433d4a3c591b39b41c7fd
Atomie CHEN <atomic_cwh@163.com>
2022-11-05 20:40:06 +0800

    Update improved gpu-jupyter docker image

--- jupyter_gpu/start_docker.sh
+++ jupyter_gpu/start_docker.sh
@@ -1,3 +1,4 @@
 docker stop context-jupyter
 docker rm context-jupyter
-docker run --gpus all -d -it --name context-jupyter --restart always -p 29503:8888 -v $(pwd)/data:/home/jovyan -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes -e NOTEBOOK_ARGS="--collaborative" --user root cschranz/gpu-jupyter:v1.4_cuda-11.2_ubuntu-20.04_slim
+# docker run --gpus all -d -it --name context-jupyter --restart always -p 29503:8888 -v $(pwd)/data:/home/jovyan -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes -e NOTEBOOK_ARGS="--collaborative" --user root cschranz/gpu-jupyter:v1.4_cuda-11.2_ubuntu-20.04_slim
+docker run --gpus all -d -it --name context-jupyter --restart always -p 29503:8888 -v $(pwd)/data:/home/jovyan -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes -e NOTEBOOK_ARGS="--collaborative" --user root atomie/gpu-jupyter:v1.4_cuda-11.2_ubuntu-20.04_slim_updated
